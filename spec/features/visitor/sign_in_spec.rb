931a430c49dc5ba1508bb53a613e041dc4863cbf
Arthur <galiullinko@gmail.com>
2016-07-26 12:25:59 +0300

    Added Capybara visitor sign in test

--- spec/features/visitor/sign_in_spec.rb
+++ spec/features/visitor/sign_in_spec.rb
@@ -1,8 +1,18 @@
 require "rails_helper"
 
-feature "Sign In" do
-  describe "Sign in as visitor" do
-    it "signs me in" do
+feature "Sign in" do
+
+  def
+
+  scenario "Sign in with correct credentials" do
+    visit "/users/sign_in/"
+    within("#new_user") do
+      fill_in "user_email", :with => "galiullinko@gmail.com"
+      fill_in "user_password", :with => "123456"
     end
+    #save_and_open_page
+    click_button "Log in"
+    expect(page).to have_content "Home"
   end
+
 end
