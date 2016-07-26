454ea66d0866544bf4c9882feb005a49fa6ebdc5
Arthur <galiullinko@gmail.com>
2016-07-26 15:10:58 +0300

    Tests

---
+++ spec/features/user/sign_out_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+feature "Sign out as user" do
+  scenario "Sign out" do
+    visit("/")
+    click_button "Sign out"
+    expect(user).to have_content "Sign in"
+  end
+end
--- spec/features/visitor/sign_in_spec.rb
+++ spec/features/visitor/sign_in_spec.rb
@@ -1,18 +1,12 @@
 require "rails_helper"
 
 feature "Sign in" do
-
-  def
-
   scenario "Sign in with correct credentials" do
     visit "/users/sign_in/"
-    within("#new_user") do
-      fill_in "user_email", :with => "galiullinko@gmail.com"
-      fill_in "user_password", :with => "123456"
-    end
-    #save_and_open_page
+    fill_in "user_email", :with => "galiullinko@gmail.com"
+    fill_in "user_password", :with => "123456"
+    save_and_open_page
     click_button "Log in"
     expect(page).to have_content "Home"
   end
-
 end
