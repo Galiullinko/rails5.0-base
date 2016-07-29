051191cc6852cae9c7e501b1b12317dc84d61e57
Arthur <galiullinko@gmail.com>
2016-07-29 16:19:20 +0300

    Now it tests, what it has to

--- spec/features/visitor/reset_password_spec.rb
+++ spec/features/visitor/reset_password_spec.rb
@@ -1,16 +1,29 @@
 require "rails_helper"
 
-feature "Sign in", js: true do
+feature "Password Reset" do
+  let(:new_password) { "sosipisos" }
   let!(:user) { create :user }
 
-  scenario "Sign in with correct credentials" do
-    visit new_user_session_path
+  scenario "Visitor resets his password" do
+    visit new_user_password_path
 
-    fill_in("user_email", with: user.email)
-    fill_in("user_password", with: user.password)
+    fill_in "Email", with: user.email
+    click_button "Send me reset password instructions"
 
-    click_on "Log in"
+    open_email(user.email)
 
-    expect(page).to have_content "Home"
+    expect(current_email).to have_subject("Reset password instructions")
+    expect(current_email).to have_body_text(user.full_name)
+
+    visit_in_email("Change my password")
+    update_password
+
+    expect(page).to have_content("Home")
+  end
+
+  def update_password
+    fill_in "Password", with: new_password
+    fill_in "Password confirmation", with: new_password
+    click_button "Update password"
   end
 end
