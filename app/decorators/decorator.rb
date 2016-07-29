569556bb05efd4ed5db07845b606e1a479b855b4
Arthur <galiullinko@gmail.com>
2016-07-29 16:23:03 +0300

    Added method_missing for working @record methods

--- app/decorators/decorator.rb
+++ app/decorators/decorator.rb
@@ -4,4 +4,12 @@ class Decorator
   def initialize(record)
     @record = record
   end
+
+  # class << self
+  #   alias_method :decorate, :new
+  # end
+
+  def method_missing(method, *args, &block)
+    @record.send(method, *args, &block)
+  end
 end
